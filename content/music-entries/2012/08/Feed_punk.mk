86203313a9340977d943509dcbcc23e0434cd8cb
uberj <uberj@onid.oregonstate.edu>
2012-11-20 02:20:53 +0000

    posts

--- content/music-entries/2012/08/Feed_punk.mk
+++
@@ -1,9 +0,0 @@
-title: Music
-type: music-entry
-category: music
-date: 2012-08-29
----
-
-The first 1:40 of this song are awesome and remind me of Daft Punk.
-
-<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F56973845"></iframe>
